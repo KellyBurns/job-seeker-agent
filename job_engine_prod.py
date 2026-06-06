733bdc9304ea26147a6aa277ef7350fae947a575
Kelly Burns <kburnsPDX@gmail.com>
2026-06-06 12:04:48 -0700

    Update job_engine_prod.py

--- job_engine_prod.py
+++ job_engine_prod.py
@@ -26,14 +26,14 @@ def build_url(base_url, query_param="q="):
 
 # Portals with restored keywords
 PORTALS = [
-    {"company": "Optum / UHG", "url": "https://careers.unitedhealthgroup.com/search-jobs?acm=ALL&alrpm=ALL&ascf=[%7B%22key%22:%22custom_fields.WorkSetting%22,%22value%22:%22Remote+-+Linked+to+Site%22%7D,%7B%22key%22:%22custom_fields.WorkSetting%22,%22value%22:%22Remote+-+Multi+State%22%7D,%7B%22key%22:%22custom_fields.WorkSetting%22,%22value%22:%22Remote+-+Specific+state+(Contractually+Required)%22%7D,%7B%22key%22:%22custom_fields.WorkSetting%22,%22value%22:%22Remote+(Nationwide)%22%7D,%7B%22key%22:%22custom_fields.WorkSetting%22,%22value%22:%22Remote%22%7D]"},
+    {"company": "Optum / UHG", "url": f"https://careers.unitedhealthgroup.com/search-jobs?acm=ALL&alrpm=ALL&ascf=[%7B%22key%22:%22custom_fields.WorkSetting%22,%22value%22:%22Remote+-+Linked+to+Site%22%7D,%7B%22key%22:%22custom_fields.WorkSetting%22,%22value%22:%22Remote+-+Multi+State%22%7D,%7B%22key%22:%22custom_fields.WorkSetting%22,%22value%22:%22Remote+-+Specific+state+(Contractually+Required)%22%7D,%7B%22key%22:%22custom_fields.WorkSetting%22,%22value%22:%22Remote+(Nationwide)%22%7D,%7B%22key%22:%22custom_fields.WorkSetting%22,%22value%22:%22Remote%22%7D]"},
     {"company": "CVS Health", "url": f"https://jobs.cvshealth.com/us/en/search-results?keywords={HEALTHCARE_AI_QUERY}&p=Aetna&cfWorkLocation=Remote"},
-    {"company": "Amgen", "url": "https://careers.amgen.com/en/search-jobs?acm=ALL&alrpm=ALL&ascf=[%7B%22key%22:%22custom_fields.CareerOpportunities%22,%22value%22:%22Digital+Innovation+and+Technology%22%7D]&k=Product"},
+    {"company": "Amgen", "url": f"https://careers.amgen.com/en/search-jobs?acm=ALL&alrpm=ALL&ascf=[%7B%22key%22:%22custom_fields.CareerOpportunities%22,%22value%22:%22Digital+Innovation+and+Technology%22%7D]&k=Product"},
     {"company": "Elevance Health", "url": f"https://careers.elevancehealth.com/jobs?keyword={HEALTHCARE_AI_QUERY}&location=Remote&page_number=1"},
     {"company": "Humana", "url": f"https://careers.humana.com/us/en/c/technology-and-digital-analytics-jobs?keywords={HEALTHCARE_AI_QUERY}"},
     {"company": "Cigna", "url": f"https://jobs.thecignagroup.com/us/en/search-results?keywords={HEALTHCARE_AI_QUERY}&from=0&num=20&gclocation=United%20States&cfWorkLocation=Remote"},
-    {"company": "Blue Cross Blue Shield", "url": "https://www.bcbs.com/careers"},
-    {"company": "Mayo Clinic", "url": "https://jobs.mayoclinic.org/search-jobs/Remote/33647/2/1000000000100/0/0/50/2"}
+    {"company": "Blue Cross Blue Shield", "url": f"https://www.bcbs.com/careers"},
+    {"company": "Mayo Clinic", "url": f"https://jobs.mayoclinic.org/search-jobs/Remote/33647/2/1000000000100/0/0/50/2"}
 ]
 
 # ==========================================
